d4bf4efc9f1828b473f78cf1b69bf0e51302cccf
Ravindra Pal <ravindrapal491@gmail.com>
2026-08-30 20:19:35 +0530

    Day-02:  15 Begineer Practice Questions Complete
    I added 8 more questions.

--- CASE_CTE.sql
+++ CASE_CTE.sql
@@ -251,3 +251,172 @@ CASE
     WHEN A.QuantityAvailable>A.ReorderLevel THEN 'Sufficient Stock'
 END AS InventoryStatus
 FROM Inventory AS A
+
+--5. The Finance department wants to calculate the total value of each order.Display: OrderID, TotalOrderValue. Use a Common Table Expression (CTE).
+;WITH OrderTotals AS
+(
+     SELECT A.OrderID,SUM(A.Quantity*A.UnitPrice) AS TotalOrderValue 
+     FROM OrderDetails AS A
+     GROUP BY A.OrderID
+)
+SELECT B.OrderID,B.TotalOrderValue FROM OrderTotals AS B
+ORDER BY B.OrderID;
+
+--6. The logistics department wants a report that displays a user-friendly shipment status instead of the actual database values.
+-- Display the following columns: ShipmentID, OrderID, ShipmentStatus, StatusDescription.
+-- Use the following rules:
+-- Delivered then Order Successfully Delivered
+-- In Transit then On the Way
+-- Pending then Awaiting Dispatch
+-- Otherwise then Unknown Status
+SELECT A.OrderID,A.ShipmentStatus,A.ShipmentID,
+CASE 
+     WHEN A.ShipmentStatus='Delivered' THEN 'Order Successfully Delivered'
+     WHEN A.ShipmentStatus='In Transit' THEN 'On the way'
+     WHEN A.ShipmentStatus='Pending' THEN 'Awaiting Dispatch'
+     ELSE 'Unknown Status'
+END AS StatusDescription
+FROM Shipments AS A
+
+--7. The Human Resources department wants to classify employees according to their years of service.
+-- Display: EmployeeID, FirstName, HireDate, YearsOfExperience, ExperienceLevel
+-- Use the following rules:
+-- Less than 2 years then Fresher
+-- Between 2 and 4 years then Experienced
+-- More than 4 years then Senior
+SELECT A.EmployeeID,A.FirstName,A.DireDate,DATEDIFF(YEAR,A.DireDate,GETDATE()) AS YearsOfExperience,
+CASE 
+     WHEN DATEDIFF(YEAR,A.DireDate,GETDATE())<2 THEN 'Fresher'
+     WHEN DATEDIFF(YEAR,A.DireDate,GETDATE()) BETWEEN 2 AND 4 THEN 'Experienced'
+     ELSE 'Senior'
+END AS ExperienceLevel
+FROM Employees AS A
+
+--8. The Sales department wants to know the total quantity sold for each product.
+-- Display: ProductID, TotalQuantitySold. Use a Common Table Expression (CTE).
+;WITH QuantitySold AS
+(
+SELECT A.ProductID,SUM(A.Quantity) AS TotalQuantitySold
+FROM OrderDetails AS A
+GROUP BY A.ProductID
+)
+SELECT B.ProductID,B.TotalQuantitySold FROM QuantitySold AS B
+
+--9. Management wants to identify repeat customers who have placed more than one order.
+-- Display: CustomerID, TotalOrders. Use a CTE.
+;WITH CustomerOrders AS 
+(
+SELECT A.CustomerID,COUNT(A.OrderID) AS TotalOrders FROM Orders AS A GROUP BY A.CustomerID
+)
+SELECT B.CustomerID,B.TotalOrders FROM CustomerOrders AS B
+WHERE TotalOrders>1;
+
+--10. The Finance department wants to classify orders based on their total value.
+-- Rules:
+-- Less than 20000 then Low Value
+-- 20000 the 50000 then Medium Value
+-- Greater than 50000 then High Value
+-- Display: OrderID, TotalOrderValue, OrderCategory
+WITH TotalOrders AS
+(
+SELECT A.OrderID,SUM(A.UnitPrice*A.Quantity) AS TotalOrderValue
+FROM OrderDetails AS A
+GROUP BY A.OrderID
+)
+SELECT B.OrderID,B.TotalOrderValue,
+CASE 
+    WHEN B.TotalOrderValue<20000 THEN 'Low Value'
+    WHEN B.TotalOrderValue BETWEEN 20000 AND 50000 THEN 'Medium Value'
+    WHEN B.TotalOrderValue>50000 THEN 'High Value'
+END AS OrderCategory
+FROM TotalOrders AS B
+
+--11. The Human Resource department wants to generate a salary report that classifies employees into different salary bands.
+--  Display the following columns: EmployeeID, FirstName, DepartmentID, Salary, SalaryBand
+-- Salary Band Rules:
+-- Salary<45000 then Band C
+-- Salary Between 45000 and 60000 then Band B
+-- Salary > 60000 then Band A
+-- Use a Common Table Expression (CTE).
+;WITH EmployeeSalary AS
+(
+SELECT A.EmployeeID,A.FirstName,A.DepartmentID,A.Salary FROM Employees AS A
+)
+SELECT B.EmployeeID,B.FirstName,B.DepartmentID,B.Salary,
+CASE 
+    WHEN B.Salary<45000 THEN 'Band C'
+    WHEN B.Salary BETWEEN 45000 AND 60000 THEN 'Band B'
+    WHEN B.Salary >60000 THEN 'Band A'
+END AS SalaryBand
+FROM EmployeeSalary AS B;
+
+--12. The Inventory Management department wants to classify products according to the quantity available in stock.
+-- Display: ProductID, QuantityAvailable, InventoryCategory.
+-- Rules:
+-- QuantityAvailable<20 then Low Stock
+-- QuantityAvailable between 20 and 100 then Medium Stock
+-- QuantityAvailable >100 then High Stock
+SELECT A.ProductID,A.QuantityAvailable,
+CASE 
+     WHEN A.QuantityAvailable<20 THEN 'Low Stock'
+     WHEN A.QuantityAvailable BETWEEN 20 AND 100 THEN 'Medium Stock'
+     WHEN A.QuantityAvailable >100 THEN 'High Stock'
+END AS InventoryCategory
+FROM Inventory AS A
+ORDER BY A.QuantityAvailable DESC;
+
+--13. The Finance department wants a monthly sales report showing the total sales amount for each month.
+-- Display: SalesYear, SalesMonth, TotalSales. Use a Common Table Expression (CTE).
+WITH MonthlySales AS
+(
+SELECT YEAR(A.OrderDate) AS SalesYear,MONTH(A.OrderDate) AS SalesMonth,SUM(B.Quantity*B.UnitPrice) AS TotalSales 
+FROM Orders AS A
+INNER JOIN OrderDetails AS B
+ON A.OrderID=B.OrderID
+GROUP BY YEAR(A.OrderDate),MONTH(A.OrderDate)
+)
+SELECT C.SalesYear,C.SalesMonth,C.TotalSales FROM MonthlySales AS C
+ORDER BY C.SalesYear,C.SalesMonth;
+
+--14. Management wants to classify products according to the total quantity sold.
+-- Rule:
+-- Quantity Sold < 3 then Low Selling
+-- Quantity Sold between 3 and 8 then Moderate Selling
+-- Quantity Sold < 8 then Best Seller
+-- Display: ProductID, TotalQuantitySold, PerformanceCategory
+WITH ProductSales AS
+(
+SELECT A.ProductID,SUM(A.Quantity) AS TotalQuantitySold FROM OrderDetails AS A GROUP BY A.ProductID
+)
+SELECT B.ProductID,B.TotalQuantitySold,
+CASE 
+     WHEN B.TotalQuantitySold<3 THEN 'Low Selling'
+     WHEN B.TotalQuantitySold BETWEEN 3 AND 8 THEN 'Moderate Selling'
+     WHEN B.TotalQuantitySold >8 THEN 'Best Seller'
+END AS PerformanceCategory
+FROM ProductSales AS B
+ORDER BY B.TotalQuantitySold DESC;
+
+--15. The Sales Director wants  a report showing each order with its customer name, total order value and business category.
+-- Rules:
+-- Less than 20000 then Small Order
+-- Between 20000 and 50000 then Medium Order
+-- Greater than 50000 then Large Order
+-- Display: OrderID, CustomerName, TotalOrderValue, OrderCategory.
+WITH OrderSummary AS
+(
+SELECT A.OrderID,B.CustomerName,SUM(C.Quantity*C.UnitPrice) AS TotalOrderValue FROM Orders AS A
+INNER JOIN Customers AS B
+ON A.CustomerID=B.CustomerID
+INNER JOIN OrderDetails AS C
+ON A.OrderID=C.OrderID
+GROUP BY A.OrderID,B.CustomerName
+)
+SELECT D.OrderID,D.CustomerName,D.TotalOrderValue,
+CASE
+     WHEN D.TotalOrderValue<20000 THEN 'Small Order'
+     WHEN D.TotalOrderValue BETWEEN 20000 AND 50000 THEN 'Medium Order'
+     WHEN D.TotalOrderValue>50000 THEN 'Large Order'
+END AS OrderCategory
+FROM OrderSummary AS D
+ORDER BY D.TotalOrderValue DESC;
